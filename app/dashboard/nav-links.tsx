d8081bf815b3baa30c15bf76cc3ce65fa4853c20
vatsal-bhatti <vatsal.bhatti@bacancy.com>
2024-05-23 12:46:44 +0530

    till 5th chapter

---
+++ app/dashboard/nav-links.tsx
@@ -0,0 +1,61 @@
+import {
+    UserGroupIcon,
+    HomeIcon,
+    DocumentDuplicateIcon,
+  } from '@heroicons/react/24/outline';
+ import { usePathname } from 'next/navigation';
+  import Link from 'next/link';
+  import clsx from 'clsx';
+   
+  // ...
+   
+  export default function NavLinks() {
+    const pathname = usePathname();
+
+
+
+
+const links = [{
+    name:"link1",
+    icon:"icon",
+href:"href"
+},{
+    name:"link1",
+    icon:"icon",
+href:"href"
+},{
+    name:"link1",
+    icon:"icon",
+href:"href"
+},{
+    name:"link1",
+    icon:"icon",
+href:"href"
+}]
+
+    return (
+      <>
+        {links.map((link) => {
+          const LinkIcon = link.icon;
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={clsx(
+                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                {
+                  'bg-sky-100 text-blue-600': pathname === link.href,
+                },
+              )}
+            >
+              <LinkIcon className="w-6" />
+              <p className="hidden md:block">{link.name}</p>
+            </Link>
+          );
+        })}
+      </>
+    );
+        })}
+      </>
+    );
+  }
